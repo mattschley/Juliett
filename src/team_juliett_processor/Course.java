e74add410d99c0ddbadd73937a3000a796df8833
Ethan Robison <ethanCrobison@gmail.com>
2014-05-12 11:19:29 -0500

    Course Class Rough

--- src/team_juliett_processor/Course.java
+++ src/team_juliett_processor/Course.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package team_juliett_processor;
 
 /**
@@ -11,16 +10,35 @@ package team_juliett_processor;
  * @author Ethan
  */
 public class Course {
+
     // beginning and end of class--formatted HHMM
     private int start_time;
     private int end_time;
-    
+
+    // days of the week on which the class is held (binary format)
+    private int days_of_week;
+
     // course ID (not to be confused with course number)
     private int course_id;
+
+    // course number
+    private int course_number;
+
+    // location on campus (North, Central, or South as 1, 0, -1, respectively)
+    private int location;
     
+// parameters (area 1 distro, etc.)
+    // TO-DO
+
+    // pre-requisites
     
+// major/minor requirements
+
+    /*------------------------------------------------------------------------*/
     
-    
+// constructor
+    public Course() {
+
+    }
 
 }
-

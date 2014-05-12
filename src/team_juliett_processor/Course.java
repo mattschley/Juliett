0be9218dd43f817931a5254f0d563530e04f652d
Ethan Robison <ethanCrobison@gmail.com>
2014-05-12 11:29:24 -0500

    Course Class Rough

--- src/team_juliett_processor/Course.java
+++ src/team_juliett_processor/Course.java
@@ -10,25 +10,27 @@ package team_juliett_processor;
  * @author Ethan
  */
 public class Course {
-
-    // beginning and end of class--formatted HHMM
+    
+// beginning and end of class--formatted HHMM
     private int start_time;
     private int end_time;
-
-    // days of the week on which the class is held (binary format)
+    
+// days of the week on which the class is held (binary format)
     private int days_of_week;
-
-    // course ID (not to be confused with course number)
+    
+// course ID (not to be confused with course number)
     private int course_id;
-
-    // course number
+    
+// course number
     private int course_number;
-
-    // location on campus (North, Central, or South as 1, 0, -1, respectively)
+    
+// location on campus (North, Central, or South as 1, 0, -1, respectively)
     private int location;
     
-// parameters (area 1 distro, etc.)
-    // TO-DO
+// parameters (area 1 distro, etc.)    
+// bool 6 long array
+    private boolean[] distros;
+    
 
     // pre-requisites
     
@@ -38,7 +40,20 @@ public class Course {
     
 // constructor
     public Course() {
-
+        
     }
 
-}
+    /*------------------------------------------------------------------------*/    
+
+// return the start and end times
+    public int[] get_times() {
+        
+        int [] x;
+        x = new int[2];
+        x[0] = start_time;
+        x[1] = end_time;
+        
+        return x;
+    }
+
+}   // end definition

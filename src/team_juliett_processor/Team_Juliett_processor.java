4292756cfeb4ef50c7339d632f7201c1c35f69b5
Tali Laifer <tlaifer317@gmail.com>
2014-05-23 21:26:30 -0500

    Merge origin/master

--- src/team_juliett_processor/Team_Juliett_processor.java
+++ src/team_juliett_processor/Team_Juliett_processor.java
@@ -24,24 +24,22 @@ public class Team_Juliett_processor {
         //eecs meets MWF 12-1250 course id 102
         //history meets TTh 1230 - 150 course id 103
         //banana meets MWF 9-950 course id 104
-        Course math = new Course       ("math", 900, 950,  10101, 100);
-        Course english = new Course("english", 1100, 1150, 10101, 101);
-        Course eecs = new Course      ("eecs", 1200, 1250, 10101, 105);
+        Course math = new Course       ("math", 900, 950, 100);
+        Course english = new Course("english", 1100, 1150, 101);
+        Course eecs = new Course      ("eecs", 1200, 1250, 105);
         
-        Course history = new Course ("history", 1230, 150, 01010, 102);
-        Course science = new Course  ("science",1230, 150, 01010, 103);
-        Course gym = new Course          ("gym", 200, 320, 01010, 104);
-        
-        
-       
+        Course history = new Course ("history", 1230, 150, 102);
+        Course science = new Course  ("science",1230, 150, 103);
+        Course gym = new Course          ("gym", 200, 320, 104);
         
         Course[] courselist;
-        courselist = new Course[5];
+        courselist = new Course[6];
         courselist[0] = math;
         courselist[1] = english;
         courselist[2] = history;
         courselist[3] = science;
         courselist[4] = eecs;
+        courselist[5] = gym;
         
         Course mandatory[] = {math, history, science};
         Course desired[] = {english, eecs, gym};
@@ -49,7 +47,7 @@ public class Team_Juliett_processor {
         int hardsofttimes[] = {800, 800};
     
         
-        for (int i = 0; i<=4; i++){
+        for (int i = 0; i<=5; i++){
             System.out.println(courselist[i].toString());
             number_of_courses++;
         }
@@ -61,9 +59,9 @@ public class Team_Juliett_processor {
         }
         System.out.println(math.toString()+ " starts at " + coursetimes[0][0] + " and ends at " + coursetimes[0][1] );
        
-        User jimbo = new User (1, mandatory, desired, optional, 800, 600, hardsofttimes);
-        System.out.println(math.conflict(science));
-        System.out.println(math.conflict(eecs));
-        System.out.println(history.conflict(science));
+      
+        for (int i = 1; i<mandatory.length; i++){
+            System.out.println(mandatory[0].conflict(courselist[i]));
+        }
          
 }

d987ff76787eef8ba7ec820abeeb096936a09494
Ethan Robison <ethanCrobison@gmail.com>
2014-05-27 17:43:33 -0500

    Fixed Some Errors

--- src/team_juliett_processor/Team_Juliett_processor.java
+++ src/team_juliett_processor/Team_Juliett_processor.java
@@ -24,47 +24,48 @@ public class Team_Juliett_processor {
         //eecs meets MWF 12-1250 course id 102
         //history meets TTh 1230 - 150 course id 103
         // meets MWF 9-950 course id 104
-        Course math = new Course       ("math", 900, 950, 100);
-        Course english = new Course("english", 1100, 1150, 101);
-        Course eecs = new Course      ("eecs", 1200, 1250, 105);
-        
-        Course history = new Course ("history", 1230, 150, 102);
-        Course science = new Course  ("science",1230, 150, 103);
-        Course gym = new Course          ("gym", 200, 320, 104);
-        
-        Course[] courselist;
-        courselist = new Course[6];
-        courselist[0] = math;
-        courselist[1] = english;
-        courselist[2] = history;
-        courselist[3] = science;
-        courselist[4] = eecs;
-        courselist[5] = gym;
-        
-        Course mandatory[] = {math, history, science};
-        Course desired[] = {english, eecs, gym};
-        Course optional[] = {science};
-        int hardsofttimes[] = {800, 800};
-    
-        
-        for (int i = 0; i<=5; i++){
-            System.out.println(courselist[i].toString());
-            number_of_courses++;
-        }
-        
-        int [][] coursetimes;
-        coursetimes = new int[number_of_courses][2];
-        for (int i = 0; i<number_of_courses; i++){
-            coursetimes[i] = courselist[i].get_times();
-        }
-        System.out.println(math.toString()+ " starts at " + coursetimes[0][0] + " and ends at " + coursetimes[0][1] );
-       
-        math.setDaysofWeek("MoWedFri");
-        gym.setDaysofWeek("MoWedFri"); 
-        System.out.println(math.getDaysofWeek());
-        
-        for (int i = 1; i<mandatory.length; i++){
-            System.out.println(mandatory[0].conflict(courselist[i]));
-        }
-         
+//        Course math = new Course       ("math", 900, 950, 100);
+//        Course english = new Course("english", 1100, 1150, 101);
+//        Course eecs = new Course      ("eecs", 1200, 1250, 105);
+//        
+//        Course history = new Course ("history", 1230, 150, 102);
+//        Course science = new Course  ("science",1230, 150, 103);
+//        Course gym = new Course          ("gym", 200, 320, 104);
+//        
+//        Course[] courselist;
+//        courselist = new Course[6];
+//        courselist[0] = math;
+//        courselist[1] = english;
+//        courselist[2] = history;
+//        courselist[3] = science;
+//        courselist[4] = eecs;
+//        courselist[5] = gym;
+//        
+//        Course mandatory[] = {math, history, science};
+//        Course desired[] = {english, eecs, gym};
+//        Course optional[] = {science};
+//        int hardsofttimes[] = {800, 800};
+//    
+//        
+//        for (int i = 0; i<=5; i++){
+//            System.out.println(courselist[i].toString());
+//            number_of_courses++;
+//        }
+//        
+//        int [][] coursetimes;
+//        coursetimes = new int[number_of_courses][2];
+//        for (int i = 0; i<number_of_courses; i++){
+//            coursetimes[i] = courselist[i].get_times();
+//        }
+//        System.out.println(math.toString()+ " starts at " + coursetimes[0][0] + " and ends at " + coursetimes[0][1] );
+//       
+//        math.setDaysofWeek("MoWedFri");
+//        gym.setDaysofWeek("MoWedFri"); 
+//        System.out.println(math.getDaysofWeek());
+//        
+//        for (int i = 1; i<mandatory.length; i++){
+//            System.out.println(mandatory[0].conflict(courselist[i]));
+//        }
+//         
+    }
 }

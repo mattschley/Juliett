8763827dd990d82af5cddbeaaee8d7bf0001b713
Ethan Robison <ethanCrobison@gmail.com>
2014-05-27 20:15:58 -0500

    Fixed Some Errors

--- src/team_juliett_processor/Team_Juliett_processor.java
+++ src/team_juliett_processor/Team_Juliett_processor.java
@@ -6,6 +6,10 @@
 
 package team_juliett_processor;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
  *
  * @author Ethan
@@ -17,55 +21,28 @@ public class Team_Juliett_processor {
      */
     private static int number_of_courses;
     
-    public static void main(String[] args) {
-
-        //math meets MWF 9-950; course id 100
-        //english meets MWF 11-1150 course id 101
-        //eecs meets MWF 12-1250 course id 102
-        //history meets TTh 1230 - 150 course id 103
-        // meets MWF 9-950 course id 104
-//        Course math = new Course       ("math", 900, 950, 100);
-//        Course english = new Course("english", 1100, 1150, 101);
-//        Course eecs = new Course      ("eecs", 1200, 1250, 105);
-//        
-//        Course history = new Course ("history", 1230, 150, 102);
-//        Course science = new Course  ("science",1230, 150, 103);
-//        Course gym = new Course          ("gym", 200, 320, 104);
-//        
-//        Course[] courselist;
-//        courselist = new Course[6];
-//        courselist[0] = math;
-//        courselist[1] = english;
-//        courselist[2] = history;
-//        courselist[3] = science;
-//        courselist[4] = eecs;
-//        courselist[5] = gym;
-//        
-//        Course mandatory[] = {math, history, science};
-//        Course desired[] = {english, eecs, gym};
-//        Course optional[] = {science};
-//        int hardsofttimes[] = {800, 800};
-//    
-//        
-//        for (int i = 0; i<=5; i++){
-//            System.out.println(courselist[i].toString());
-//            number_of_courses++;
-//        }
-//        
-//        int [][] coursetimes;
-//        coursetimes = new int[number_of_courses][2];
-//        for (int i = 0; i<number_of_courses; i++){
-//            coursetimes[i] = courselist[i].get_times();
-//        }
-//        System.out.println(math.toString()+ " starts at " + coursetimes[0][0] + " and ends at " + coursetimes[0][1] );
-//       
-//        math.setDaysofWeek("MoWedFri");
-//        gym.setDaysofWeek("MoWedFri"); 
-//        System.out.println(math.getDaysofWeek());
-//        
-//        for (int i = 1; i<mandatory.length; i++){
-//            System.out.println(mandatory[0].conflict(courselist[i]));
-//        }
-//         
+    public static void main(String[] args) throws IOException{
+        
+        BufferedReader br = new BufferedReader(new FileReader("C:/EECS_DB.txt"));
+        
+        String line;
+        
+        while((line = br.readLine()) != null) {
+            System.out.println(line);
+        }
+        br.close();
+        
+        User bob;
+        int[] mands;
+        int[] options;
+        mands = new int[3];
+        options = new int[3];
+        
+        mands[0] = mands[1] = mands[2] = options[0] = options[1] = options[2] = 3;
+        
+        bob = new User(1, mands, options, 0, 1);
+        
+        System.out.println(bob.test());
+   
     }
 }

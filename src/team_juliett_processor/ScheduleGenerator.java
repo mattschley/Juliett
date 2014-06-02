fbad576b55f60d6bcf91afa94558a482c7fa9f9c
jmidvidy <jmidvidy@gmail.com>
2014-06-01 19:54:52 -0500

    ScheduleGenerator class and location rankings

---
+++ src/team_juliett_processor/ScheduleGenerator.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package team_juliett_processor;
+
+/**
+ *
+ * @author Jeremymidvidy
+ */
+public class ScheduleGenerator 
+{
+    //a list of schedules that are can feasibly made
+    private schedule[] validSchedules;
+    private int numOfMandClasses;
+    
+    //Initalizes the list of schedules that must be passed from the main function
+    public ScheduleGenerator(schedule[] VS)
+    {
+        validSchedules = VS;
+    }
+    
+    public void RankSchedules(schedule[] s)
+    {
+        for(int i = 0; i < s[i].schedule_courses.length; i++) //change this
+        {
+            //Check what has the most number of mandatory classes - highest rank automatically
+            
+        }
+    }
+    
+}
+
+
